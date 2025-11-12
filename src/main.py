a35a340708bbeba4821b19b0311c5accda6db355
Arcumenn <steinar.graessel@gmx.net>
2025-11-12 15:03:19 +0100

    update files

---
+++ src/main.py
@@ -0,0 +1,104 @@
+import igraph as ig
+import pandas as pd
+
+from utils import *
+
+if __name__ == "__main__":
+    graph = load_graph("./sampled_data/2260916_retweet.graphml")
+
+    summary = summarise_network(graph, name="20000_retweets", betweenness=True)
+    print_summary(summary, to_file=True)
+
+    # draw_graph(graph, output="./plots/20000_retweets_betweenness.png", scale_with_degree=False)
+    # draw_graph(graph, output="./plots/20000_retweets_degree.png", scale_with_degree=True)
+    
+
+    # # top 10 actors, in measures of degree and betweenness
+    top_accounts = pd.read_csv("./data/accounts.tsv", sep="\t")
+    
+    degree_actors, betweenness_actors = extract_top10_actors(graph, summary, top_accounts)
+
+    with open("./top_actors/all_retweets.txt", "w") as f:
+        f.write(degree_actors[["Type", "Stance", "Lang"]].to_string() + "\n")
+        f.write("#######################################\n")
+        f.write(betweenness_actors[["Type", "Stance", "Lang"]].to_string())
+
+    # task 1.7 and onwards
+    gc = graph.connected_components().giant()
+    gc_summary = summarise_network(gc, name="Largest Component Only")
+
+    # task 2.1
+    order = gc_summary["order"]
+    p = gc.density(loops=False)
+    er1 = ig.Graph.Erdos_Renyi(n=order, p=p, directed=False, loops=False)
+    # er2 = ig.Graph.Erdos_Renyi(n=order, p=p, directed=False, loops=False)
+    # er3 = ig.Graph.Erdos_Renyi(n=order, p=p, directed=False, loops=False)
+    # er_list = [er1, er2, er3]
+
+    # er_summaries = [summarise_network(er, name=f"Erdos Renyi {ind+1}") for ind, er in enumerate(er_list)]
+    #for summary in er_summaries:
+    #    print_summary(summary, to_file=True)
+
+
+    # task 2.2: produce 3 BA networks, same order, same ap matrix as retweet network
+    # n = len(gc.vs)
+    # s = len(gc.es)
+    # m = int(s/n)
+    ba1 = ig.Graph.Barabasi(n=order, m=gc.degree(), directed=False)
+    # ba2 = ig.Graph.Barabasi(n=order, m=gc.degree(), directed=False)
+    # ba3 = ig.Graph.Barabasi(n=order, m=gc.degree(), directed=False)
+    # ba_list = [ba1, ba2, ba3]
+
+    # ba_summaries = [summarise_network(ba, name=f"Barabasi {ind+1}") for ind, ba in enumerate(ba_list)]
+    # for summary in ba_summaries:
+    #     print_summary(summary, to_file=True)
+
+    # task 2.3
+    p = gc.average_path_length() / 7
+    cc = gc.transitivity_avglocal_undirected()
+    ws1 = ig.Graph.Watts_Strogatz(
+        dim=1, size=100, nei=4, p=p, allowed_edge_types="multi"
+    )
+    # ws2 = ig.Graph.Watts_Strogatz(
+    #     dim=1, size=100, nei=4, p=p, allowed_edge_types="multi"
+    # )
+    # ws3 = ig.Graph.Watts_Strogatz(
+    #     dim=1, size=100, nei=4, p=p, allowed_edge_types="multi"
+    # )
+
+    # ws_list = [ws1, ws2, ws3]
+
+    # ws_summaries = [summarise_network(ws, name=f"Watts-Strogatz {ind+1}") for ind, ws in enumerate(ws_list)]
+    # for summary in ws_summaries:
+    #     print_summary(summary, to_file=True)
+
+    # task 2.4
+    # starting network (retweet)
+    rewired_graph = graph.connected_components().giant().copy()
+    # rewire edges. using keeping_degseq to preserve degree distr
+    num_swaps = 10 * rewired_graph.ecount()
+
+    # avoid multiple edges and loops
+    rewired_graph.rewire(num_swaps, allowed_edge_types="simple")
+
+    # get and see stats after rewiring
+    # rewired_summary = summarise_network(rewired_graph, name="Rewired Graph")
+    # print_summary(rewired_summary, to_file=True)
+
+    # task 3.1
+    for graph, name in zip([graph, er1, ba1, ws1, rewired_graph],["original", "erdos_renyi", "barabasi", "watts_strogatz","rewired_graph"]):
+        counts = random_walk_graph(graph)
+        plot_histogram(counts, name)
+    
+    # task 3.2
+    for graph, name in zip([graph, er1, ba1, ws1, rewired_graph],["original", "erdos_renyi", "barabasi", "watts_strogatz","rewired_graph"]):
+        positive_actors = information_diffusion(graph, num_iter=5, p=0.01)
+        print(name, ":\t", positive_actors)
+
+    # task 3.3
+    for graph, name in zip([graph, er1, ba1, rewired_graph],["original", "erdos_renyi", "barabasi","rewired_graph"]):
+        positive_actors = [opinion_diffusion(graph, num_positive=100, num_iter=10, opinion_change_th=0.8),
+                           opinion_diffusion(graph, num_positive=250, num_iter=10, opinion_change_th=0.8),
+                           opinion_diffusion(graph, num_positive=100, num_iter=10, opinion_change_th=0.5),
+                           opinion_diffusion(graph, num_positive=250, num_iter=10, opinion_change_th=0.5)]
+        print(name, ":\t", positive_actors)
--- src/process_tweets.py
+++ src/process_tweets.py
@@ -8,7 +8,11 @@ import pandas as pd
 from tqdm import tqdm
 
 
-def process_tweets(tweets:str="./data/tweets.dat", authors:str="./data/accounts.tsv", sample:int=2260916):
+def process_tweets(
+    tweets: str = "./data/tweets.dat",
+    authors: str = "./data/accounts.tsv",
+    sample: int = 2260916,
+):
 
     authors_df = pd.read_csv(authors, sep="\t")
     author_meta = authors_df.set_index("author_id")[["Lang", "Type", "Stance"]].to_dict(
@@ -16,7 +20,7 @@ def process_tweets(tweets:str="./data/tweets.dat", authors:str="./data/accounts.
     )
 
     # buffer to not write each line to output individually
-    stop = 0
+    processed = 0
     buffer = []
     buffer_size = 10000
     retweet_edges = []
@@ -24,13 +28,16 @@ def process_tweets(tweets:str="./data/tweets.dat", authors:str="./data/accounts.
     # count number of tweets with multiple references (might be worth keeping them in; currently ~400 removed)
     multiple_references = 0
     # with open("tweets_output.jsonl", "w") as out_file, open("../data/first1000_tweets.dat", "r") as in_file:
-    with open(f"./data/{sample}_tweets.dat", "w") as out_file, open(
+    with open(f"./sampled_data/{sample}_tweets.dat", "w") as out_file, open(
         tweets, "r"
     ) as in_file:
         for line in tqdm(in_file, total=2260916):
-            stop += 1
             row = orjson.loads(line)
-            tweet = {"id": row["id"], "text": row["text"], "date": row["created_at"][:10]}
+            tweet = {
+                "id": row["id"],
+                "text": row["text"],
+                "date": row["created_at"][:10],
+            }
             author_id = int(row["author_id"])
             if author_id in author_meta:
                 tweet["account"] = {
@@ -46,6 +53,9 @@ def process_tweets(tweets:str="./data/tweets.dat", authors:str="./data/accounts.
 
             refs = row.get("referenced_tweets")
             if not isinstance(refs, list):
+                processed += 1
+                if processed == sample:
+                    break
                 continue
             if len(refs) != 1:
                 multiple_references += 1
@@ -67,13 +77,14 @@ def process_tweets(tweets:str="./data/tweets.dat", authors:str="./data/accounts.
                     row["id"],
                     row["possibly_sensitive"],
                 )
-                if tweet_type == "retweeted":                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+                if tweet_type == "retweeted":
                     retweet_edges.append(edge)
                 else:
                     reply_edges.append(edge)
-            
-            if stop == sample:
-                break
+
+                processed += 1
+                if processed == sample:
+                    break
 
         # add tweets still remaining in buffer
         if buffer:
@@ -94,9 +105,9 @@ def process_tweets(tweets:str="./data/tweets.dat", authors:str="./data/accounts.
     print("Number of multiple references", multiple_references)
 
     # write the graphs to .graphml files
-    reply_graph.write_graphml(f"./data/{sample}_reply.graphml")
-    retweet_graph.write_graphml(f"./data/{sample}_retweet.graphml")
+    reply_graph.write_graphml(f"./sampled_data/{sample}_reply.graphml")
+    retweet_graph.write_graphml(f"./sampled_data/{sample}_retweet.graphml")
 
 
 if __name__ == "__main__":
-    process_tweets(sample=500)  
+    process_tweets(sample=20000)
